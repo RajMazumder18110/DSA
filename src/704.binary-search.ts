5a6e69ac502ab3ef91c2ea2d6fb2a63ce868b78b
Raj Mazumder <rajmazumder27.08.2001@gmail.com>
2026-02-02 12:23:42 +0530

    solved: (704) binary search

---
+++ src/704.binary-search.ts
@@ -0,0 +1,21 @@
+/*
+ * @lc app=leetcode id=704 lang=typescript
+ *
+ * [704] Binary Search
+ */
+
+// @lc code=start
+function search(nums: number[], target: number): number {
+  let start = 0;
+  let end = nums.length - 1;
+
+  while (start <= end) {
+    const mid = Math.floor((start + end) / 2);
+    if (nums[mid] === target) return mid;
+    else if (nums[mid]! < target) start++;
+    else end--;
+  }
+
+  return -1;
+}
+// @lc code=end
